ee534ee14519e97f523197257cdb0d7797dd8b3d
aib <aibok42@gmail.com>
2018-05-12 18:29:09 +0300

    Add the base ConfigItem class

---
+++ src/ConfigItem.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace MagicConfig
+{
+	// This is the base class for all the MagicConfig configuration items
+	public abstract class ConfigItem: IEquatable<ConfigItem>
+	{
+		// Thrown by Assign when a non-polymorphic item is assigned an incompatible type
+		public class InvalidTypeAssignmentException: Exception {
+			public readonly ConfigItem OldItem, NewItem;
+			public InvalidTypeAssignmentException(ConfigItem oldItem, ConfigItem newItem)
+				{ OldItem = oldItem; NewItem = newItem; }
+		}
+
+		public abstract bool Equals(ConfigItem item);
+
+		// Non-polymorphic configuration item types might throw InvalidTypeAssignment
+		public abstract void Assign(ConfigItem item);
+	}
+}
