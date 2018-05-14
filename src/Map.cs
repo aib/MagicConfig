98668d35a2cd7fbc9cc18ef09da11851e02800ca
aib <aibok42@gmail.com>
2018-05-14 12:33:34 +0300

    Make Map a generic interface

--- src/Map.cs
+++ src/Map.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 
 namespace MagicConfig
 {
-	public abstract class Map: ConfigItem
+	public abstract class Map<T>: ConfigItem
 	{
 		protected abstract IEnumerable<string> _mapKeys();
-		protected abstract ConfigItem _mapGet(string key);
-		protected abstract void _mapSet(string key, ConfigItem value);
+		protected abstract T _mapGet(string key);
+		protected abstract void _mapSet(string key, T value);
 		protected abstract void _mapDel(string key);
 	}
 }
