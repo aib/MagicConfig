8a0e12e0baecb9d7af6b43c2dadd7539d9cf992e
aib <aibok42@gmail.com>
2018-05-17 14:54:56 +0300

    Add class KeyedItemList

---
+++ src/KeyedItemList.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+
+namespace MagicConfig
+{
+	// This class is a list of "keyed" items, items with a key getter
+	// Thus it is actually a map
+	// This list does not preserve order
+	public class KeyedItemList<T>: DynamicMap<T>, ICollection<T>, IEquatable<KeyedItemList<T>>
+		where T: ConfigItem, IKeyedItem
+	{
+		public bool Equals(KeyedItemList<T> other)
+		{
+			return !object.ReferenceEquals(other, null) && ((DynamicMap<T>) this).Equals(other);
+		}
+
+		// ICollection<T>
+		public void Add(T item) => dictionary.Add(item.GetKeyedItemKey(), item);
+		public new void Clear() => dictionary.Clear();
+		public bool Contains(T item) => dictionary.ContainsKey(item.GetKeyedItemKey());
+		public void CopyTo(T[] array, int arrayIndex) => dictionary.Values.CopyTo(array, arrayIndex);
+		public bool Remove(T item) => dictionary.Remove(item.GetKeyedItemKey());
+		public new int Count => dictionary.Count;
+		public new bool IsReadOnly => false;
+
+		// IEnumerable<T>
+		public new IEnumerator<T> GetEnumerator() => dictionary.Values.GetEnumerator();
+	}
+}
