472c460c659a96d5464a6ba33ad09416fd852277
aib <aibok42@gmail.com>
2018-05-23 12:32:50 +0300

    Reimplement SingleItem as a subclass of SingleValue

--- src/SingleItem.cs
+++ src/SingleItem.cs
@@ -4,18 +4,10 @@ namespace MagicConfig
 {
 	// This class holds a single value, usually a scalar
 	// Updates to this class destroy the old value
-	public class SingleItem<T>: ConfigItem, IEquatable<SingleItem<T>>
+	public class SingleItem<T>: SingleValue<T>, IEquatable<SingleItem<T>>
 		where T: IEquatable<T>
 	{
-		public class UpdatedArgs: EventArgs { public T OldValue; public T NewValue; }
-		public event EventHandler<UpdatedArgs> Updated;
-
-		private T value;
-
-		public SingleItem(T value)
-		{
-			this.value = value;
-		}
+		public SingleItem(T value) :base(value) {}
 
 		public static implicit operator T(SingleItem<T> s)
 		{
@@ -30,22 +22,6 @@ namespace MagicConfig
 		public bool Equals(SingleItem<T> other)
 		{
 			return !object.ReferenceEquals(other, null) && value.Equals(other.value);
-		}
-
-		public override bool Equals(ConfigItem other)
-		{
-			return (other is SingleItem<T> otherItem) && Equals(otherItem);
-		}
-
-		public override void Assign(ConfigItem other)
-		{
-			if (other is SingleItem<T> otherItem) {
-				var args = new UpdatedArgs { OldValue = value, NewValue = otherItem.value };
-				value = otherItem.value;
-				Updated?.Invoke(this, args);
-			} else {
-				throw new InvalidTypeAssignmentException(this, other);
-			}
 		}
 	}
 }
