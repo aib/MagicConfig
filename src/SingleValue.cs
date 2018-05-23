bd97dd6a89c344b8c96545ba442331dc0263dd04
aib <aibok42@gmail.com>
2018-05-23 12:30:56 +0300

    Add SingleValue as a direct copy of SingleItem without the type constraint

---
+++ src/SingleValue.cs
@@ -0,0 +1,52 @@
+using System;
+
+namespace MagicConfig
+{
+	// This class holds a single value that is not or cannot be made IEquatable -- such as an enum
+	// It is an inferior version of SingleItem, please do not use unless you have to
+	public class SingleValue<T>: ConfigItem, IEquatable<SingleValue<T>>
+	{
+		public class UpdatedArgs: EventArgs { public T OldValue; public T NewValue; }
+		public event EventHandler<UpdatedArgs> Updated;
+
+		protected T value;
+
+		public SingleValue(T value)
+		{
+			this.value = value;
+		}
+
+		public T Item => value;
+
+		public static implicit operator T(SingleValue<T> s)
+		{
+			return s.value;
+		}
+
+		public static implicit operator SingleValue<T>(T v)
+		{
+			return new SingleValue<T>(v);
+		}
+
+		public bool Equals(SingleValue<T> other)
+		{
+			return !object.ReferenceEquals(other, null) && value.Equals(other.value);
+		}
+
+		public override bool Equals(ConfigItem other)
+		{
+			return (other is SingleValue<T> otherItem) && Equals(otherItem);
+		}
+
+		public override void Assign(ConfigItem other)
+		{
+			if (other is SingleValue<T> otherItem) {
+				var args = new UpdatedArgs { OldValue = value, NewValue = otherItem.value };
+				value = otherItem.value;
+				Updated?.Invoke(this, args);
+			} else {
+				throw new InvalidTypeAssignmentException(this, other);
+			}
+		}
+	}
+}
