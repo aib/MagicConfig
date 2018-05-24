7d056a72e701b39ae6b66e5bd09dcd90b463de19
aib <aibok42@gmail.com>
2018-05-24 12:19:08 +0300

    Add a generic SingleValue<> converter

--- adapters/Newtonsoft_Json/src/SingleValueConverter.cs
+++ adapters/Newtonsoft_Json/src/SingleValueConverter.cs
@@ -3,6 +3,7 @@ using System;
 
 namespace MagicConfig.Adapters.Newtonsoft_Json
 {
+	// Converter for a single SingleValue<T> type
 	public class SingleValueConverter<T>: JsonConverter<SingleValue<T>>
 	{
 		public override void WriteJson(JsonWriter writer, SingleValue<T> value, JsonSerializer serializer)
@@ -13,6 +14,43 @@ namespace MagicConfig.Adapters.Newtonsoft_Json
 		public override SingleValue<T> ReadJson(JsonReader reader, Type objectType, SingleValue<T> existingValue, bool hasExistingValue, JsonSerializer serializer)
 		{
 			return new SingleValue<T>(serializer.Deserialize<T>(reader));
+		}
+	}
+
+	// Converter for all SingleValue<T> types
+	public class SingleValueConverter: JsonConverter
+	{
+		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+		{
+			Type itemType = findItemType(value.GetType());
+			object itemValue = value.GetType().GetProperty("Value").GetValue(value);
+
+			serializer.Serialize(writer, itemValue, itemType);
+		}
+
+		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+		{
+			Type itemType = findItemType(objectType);
+			object itemValue = ReflectionUtils.CallMethod(serializer, "Deserialize", new[] {itemType}, new[] {reader});
+
+			return typeof(SingleValue<>)
+				.MakeGenericType(itemType)
+				.GetConstructor(new[] {itemType})
+				.Invoke(new[] {itemValue});
+		}
+
+		public override bool CanConvert(Type objectType)
+		{
+			return (findItemType(objectType) != null);
+		}
+
+		private Type findItemType(Type objectType)
+		{
+			Type itemType = ReflectionUtils.FindInTypeHierarchy(
+				t => t.IsGenericType && t.GetGenericTypeDefinition() == typeof(SingleValue<>),
+				objectType);
+
+			return itemType?.GenericTypeArguments[0];
 		}
 	}
 }
