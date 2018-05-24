b7629a6833de71f82a6fb455d7c7933667927353
aib <aibok42@gmail.com>
2018-05-24 12:35:24 +0300

    Test KeyedItemList conversion to JSON and back

--- adapters/Newtonsoft_Json/tests/KeyedItemListConverterTests.cs
+++ adapters/Newtonsoft_Json/tests/KeyedItemListConverterTests.cs
@@ -17,16 +17,36 @@ namespace MagicConfig.Adapters.Newtonsoft_Json.Test
 			public const string JSON = "{ \"foos\": [ { \"name\": \"a\", \"value\": 1 }, { \"name\": \"b\", \"value\": 2 } ] }";
 		}
 
+		private void testConversion(JsonConverter[] converters)
+		{
+			{
+				var obj = JsonConvert.DeserializeObject<TestClass>(TestClass.JSON, converters);
+				Assert.Equal(2, obj.foos.Count);
+				Assert.Equal("a", obj.foos["a"].name);
+				Assert.Equal(1, (int) obj.foos["a"].value);
+				Assert.Equal("b", obj.foos["b"].name);
+				Assert.Equal(2, (int) obj.foos["b"].value);
+			}
+
+			{
+				var obj1 = new TestClass {
+					foos = new KeyedItemList<TestClass.Foo> {
+						new TestClass.Foo { name = "foo", value = 42 },
+						new TestClass.Foo { name = "bar", value = 11 }
+					}
+				};
+				string json = JsonConvert.SerializeObject(obj1, converters);
+				var obj2 = JsonConvert.DeserializeObject<TestClass>(json, converters);
+
+				Assert.True(obj1.Equals(obj2));
+				Assert.True(obj2.Equals(obj1));
+			}
+		}
+
 		[Fact]
 		public void TestKeyedItemListConverter()
 		{
-			var converters = new JsonConverter[] { new KeyedItemListConverter<TestClass.Foo>() };
-			var obj = JsonConvert.DeserializeObject<TestClass>(TestClass.JSON, converters);
-			Assert.Equal(2, obj.foos.Count);
-			Assert.Equal("a", obj.foos["a"].name);
-			Assert.Equal(1, (int) obj.foos["a"].value);
-			Assert.Equal("b", obj.foos["b"].name);
-			Assert.Equal(2, (int) obj.foos["b"].value);
+			testConversion(new JsonConverter[] { new KeyedItemListConverter<TestClass.Foo>() });
 		}
 	}
 }
