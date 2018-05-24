188305bac9a337a19c25ae49bcab88804be75c43
aib <aibok42@gmail.com>
2018-05-24 13:01:42 +0300

    Add a generic KeyedItemList<> converter

--- adapters/Newtonsoft_Json/src/KeyedItemListConverter.cs
+++ adapters/Newtonsoft_Json/src/KeyedItemListConverter.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 
 namespace MagicConfig.Adapters.Newtonsoft_Json
 {
+	// Converter for a single KeyedItemList<T> type
 	public class KeyedItemListConverter<T>: JsonConverter<KeyedItemList<T>>
 		where T: ConfigItem, IKeyedItem
 	{
@@ -15,6 +16,49 @@ namespace MagicConfig.Adapters.Newtonsoft_Json
 		public override KeyedItemList<T> ReadJson(JsonReader reader, Type objectType, KeyedItemList<T> existingValue, bool hasExistingValue, JsonSerializer serializer)
 		{
 			return new KeyedItemList<T>(serializer.Deserialize<ICollection<T>>(reader));
+		}
+	}
+
+	// Converter for all KeyedItemList<T> types
+	public class KeyedItemListConverter: JsonConverter
+	{
+		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+		{
+			Type itemType = findItemType(value.GetType());
+			Type listType = typeof(List<>).MakeGenericType(itemType);
+			Type enumerableType = typeof(IEnumerable<>).MakeGenericType(itemType);
+
+			object list = listType
+				.GetConstructor(new[] {enumerableType})
+				.Invoke(new[] {value});
+
+			serializer.Serialize(writer, list, listType);
+		}
+
+		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+		{
+			Type itemType = findItemType(objectType);
+			Type listType = typeof(List<>).MakeGenericType(itemType);
+			object itemList = ReflectionUtils.CallMethod(serializer, "Deserialize", new[] {listType}, new[] {reader});
+
+			return typeof(KeyedItemList<>)
+				.MakeGenericType(itemType)
+				.GetConstructor(new[] {listType})
+				.Invoke(new[] {itemList});
+		}
+
+		public override bool CanConvert(Type objectType)
+		{
+			return (findItemType(objectType) != null);
+		}
+
+		private Type findItemType(Type objectType)
+		{
+			Type itemType = ReflectionUtils.FindInTypeHierarchy(
+				t => t.IsGenericType && t.GetGenericTypeDefinition() == typeof(KeyedItemList<>),
+				objectType);
+
+			return itemType?.GenericTypeArguments[0];
 		}
 	}
 }
