123188318fe228b8812609e75f0722cbd3129301
aib <aibok42@gmail.com>
2018-05-24 12:21:53 +0300

    Add a generic SingleItem<> converter

--- adapters/Newtonsoft_Json/src/SingleItemConverter.cs
+++ adapters/Newtonsoft_Json/src/SingleItemConverter.cs
@@ -3,6 +3,7 @@ using System;
 
 namespace MagicConfig.Adapters.Newtonsoft_Json
 {
+	// Converter for a single SingleItem<T> type
 	public class SingleItemConverter<T>: JsonConverter<SingleItem<T>>
 		where T: IEquatable<T>
 	{
@@ -14,6 +15,43 @@ namespace MagicConfig.Adapters.Newtonsoft_Json
 		public override SingleItem<T> ReadJson(JsonReader reader, Type objectType, SingleItem<T> existingValue, bool hasExistingValue, JsonSerializer serializer)
 		{
 			return new SingleItem<T>(serializer.Deserialize<T>(reader));
+		}
+	}
+
+	// Converter for all SingleItem<T> types
+	public class SingleItemConverter: JsonConverter
+	{
+		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+		{
+			Type itemType = findItemType(value.GetType());
+			object itemValue = value.GetType().GetProperty("Value").GetValue(value);
+
+			serializer.Serialize(writer, itemValue, itemType);
+		}
+
+		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+		{
+			Type itemType = findItemType(objectType);
+			object itemValue = ReflectionUtils.CallMethod(serializer, "Deserialize", new[] {itemType}, new[] {reader});
+
+			return typeof(SingleItem<>)
+				.MakeGenericType(itemType)
+				.GetConstructor(new[] {itemType})
+				.Invoke(new[] {itemValue});
+		}
+
+		public override bool CanConvert(Type objectType)
+		{
+			return (findItemType(objectType) != null);
+		}
+
+		private Type findItemType(Type objectType)
+		{
+			Type itemType = ReflectionUtils.FindInTypeHierarchy(
+				t => t.IsGenericType && t.GetGenericTypeDefinition() == typeof(SingleItem<>),
+				objectType);
+
+			return itemType?.GenericTypeArguments[0];
 		}
 	}
 }
