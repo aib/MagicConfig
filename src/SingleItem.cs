9414b977afb513d79b8408de032b495a4445f32d
aib <aibok42@gmail.com>
2018-05-26 02:02:46 +0300

    Split SingleItem and SingleValue...
    Make SingleItem's null convert to null and SingleValue's null to default(T).
    Restrict SingleItem to reference (nullable) types.

--- src/SingleItem.cs
+++ src/SingleItem.cs
@@ -2,16 +2,26 @@ using System;
 
 namespace MagicConfig
 {
-	// This class holds a single value, usually a scalar
+	// This class holds a single reference (class) value
+	// For non-nullable, non-IEquatable types, use SingleValue
 	// Updates to this class destroy the old value
-	public class SingleItem<T>: SingleValue<T>, IEquatable<SingleItem<T>>
-		where T: IEquatable<T>
+	public class SingleItem<T>: ConfigItem, IEquatable<SingleItem<T>>
+		where T: class, IEquatable<T>
 	{
-		public SingleItem(T value) :base(value) {}
+		public class UpdatedArgs: EventArgs { public T OldValue; public T NewValue; }
+		public event EventHandler<UpdatedArgs> Updated;
+
+		private T value;
+		public T Value => value;
+
+		public SingleItem(T value)
+		{
+			this.value = value;
+		}
 
 		public static implicit operator T(SingleItem<T> s)
 		{
-			return s.value;
+			return (s == null) ? null : s.value;
 		}
 
 		public static implicit operator SingleItem<T>(T v)
@@ -22,6 +32,22 @@ namespace MagicConfig
 		public bool Equals(SingleItem<T> other)
 		{
 			return !object.ReferenceEquals(other, null) && value.Equals(other.value);
+		}
+
+		public override bool Equals(ConfigItem other)
+		{
+			return (other is SingleItem<T> otherItem) && Equals(otherItem);
+		}
+
+		public override void Assign(ConfigItem other)
+		{
+			if (other is SingleItem<T> otherItem) {
+				var args = new UpdatedArgs { OldValue = value, NewValue = otherItem.value };
+				value = otherItem.value;
+				Updated?.Invoke(this, args);
+			} else {
+				throw new InvalidTypeAssignmentException(this, other);
+			}
 		}
 	}
 }
--- src/SingleValue.cs
+++ src/SingleValue.cs
@@ -2,25 +2,25 @@ using System;
 
 namespace MagicConfig
 {
-	// This class holds a single value that is not or cannot be made IEquatable -- such as an enum
-	// It is an inferior version of SingleItem, please do not use unless you have to
+	// This class holds a single value, usually a scalar
+	// For reference types, you should prefer SingleItem
+	// Updates to this class destroy the old value
 	public class SingleValue<T>: ConfigItem, IEquatable<SingleValue<T>>
 	{
 		public class UpdatedArgs: EventArgs { public T OldValue; public T NewValue; }
 		public event EventHandler<UpdatedArgs> Updated;
 
-		protected T value;
+		private T value;
+		public T Value => value;
 
 		public SingleValue(T value)
 		{
 			this.value = value;
 		}
 
-		public T Value => value;
-
 		public static implicit operator T(SingleValue<T> s)
 		{
-			return s.value;
+			return (s == null) ? default(T) : s.value;
 		}
 
 		public static implicit operator SingleValue<T>(T v)
