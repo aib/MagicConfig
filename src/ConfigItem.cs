21350340bf3e7c23a483197dd390f7f3bc9b342e
aib <aibok42@gmail.com>
2018-06-03 15:25:36 +0300

    Change a comment to state Assign throws InvalidTypeAssignmentException for null

--- src/ConfigItem.cs
+++ src/ConfigItem.cs
@@ -5,7 +5,7 @@ namespace MagicConfig
 	// This is the base class for all the MagicConfig configuration items
 	public abstract class ConfigItem: IEquatable<ConfigItem>
 	{
-		// Thrown by Assign when a non-polymorphic item is assigned an incompatible type
+		// Thrown by Assign when a non-polymorphic item is assigned an incompatible type or null
 		public class InvalidTypeAssignmentException: Exception {
 			public readonly ConfigItem OldItem, NewItem;
 			public InvalidTypeAssignmentException(ConfigItem oldItem, ConfigItem newItem)
