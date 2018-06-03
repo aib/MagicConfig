1cfb7f8124824d457ec9ecb7a78db1c367a28c61
aib <aibok42@gmail.com>
2018-06-03 20:23:33 +0300

    Add base class InvalidAssignmentException for exceptions thrown by Assign

--- src/ConfigItem.cs
+++ src/ConfigItem.cs
@@ -5,16 +5,20 @@ namespace MagicConfig
 	// This is the base class for all the MagicConfig configuration items
 	public abstract class ConfigItem: IEquatable<ConfigItem>
 	{
-		// Thrown by Assign when a non-polymorphic item is assigned an incompatible type or null
-		public class InvalidTypeAssignmentException: Exception {
+		// Base class for exceptions thrown by Assign on invalid assignments
+		public abstract class InvalidAssignmentException: Exception {}
+
+		// Thrown when a non-polymorphic item is assigned an incompatible type or null
+		public class InvalidTypeAssignmentException: InvalidAssignmentException {
 			public readonly ConfigItem OldItem, NewItem;
 			public InvalidTypeAssignmentException(ConfigItem oldItem, ConfigItem newItem)
 				{ OldItem = oldItem; NewItem = newItem; }
 		}
 
+
 		public abstract bool Equals(ConfigItem item);
 
-		// Non-polymorphic configuration item types might throw InvalidTypeAssignment
+		// Throws InvalidAssignmentException
 		public abstract void Assign(ConfigItem item);
 	}
 }
